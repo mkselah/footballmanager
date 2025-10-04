d6e82ad2e9b31b7fed922e5ddb57e9fd854a8eac
Mohammad Salahadeen <mohammad.salahadeen@gmail.com>
2025-10-04 22:07:44 +0200

    new supabase url

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = "https://frdqkeaofjxmvvtfwgkf.supabase.co"; // REPLACE
-export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyZHFrZWFvZmp4bXZ2dGZ3Z2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5NDYxOTIsImV4cCI6MjA2MzUyMjE5Mn0.sEhuHmksq1VoOOjwY1DNazddQ0S8fXuxwfjjapIm8Zs"; // REPLACE
+export const SUPABASE_URL = "https://exqaziqinrxrvowfhnst.supabase.co"; // REPLACE
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4cWF6aXFpbnJ4cnZvd2ZobnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NzEyNjMsImV4cCI6MjA3NTE0NzI2M30.lpDNOnZFNhqcg1hvRlLyfzm9EWtseO4F0OpKT5HGQw4"; // REPLACE
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
